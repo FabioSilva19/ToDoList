b95caa77620502016281469fd9d9b0bd5b78ff5e
FabioSilva19 <fabio.ceapp@gmail.com>
2021-10-02 20:34:39 -0300

    Adicionado função de adicionar a tarefa com a Tecla enter e poder excluíla

--- ToDo list/javascript.js
+++ ToDo list/javascript.js
@@ -2,6 +2,14 @@ const Tarefa =  [
      
 ]
 i = 0;
+
+function handle(event){
+    if(event.keyCode === 13){
+        event.preventDefault(); // Ensure it is only this code that rusn
+
+        adicionarTarefa()
+    }
+}
 function adicionarTarefa(){
     
     novaTarefa = document.getElementById("tarefa").value;
@@ -33,12 +41,32 @@ function adicionarTarefa(){
 
 function escreverTarefa(){
     var paragrafo = document.createElement("p")
+    var fechar = document.createElement("span")
+    fechar.classList.add("btnFechar" + i)
+
     var texto = document.createTextNode(i+1 + " - " +Tarefa[i])
+    var iconeFechar = document.createTextNode("X")
+
     paragrafo.appendChild(texto)
+    fechar.appendChild(iconeFechar)
+    fechar.addEventListener("click", fecharBotao);
     
     divTarefas = document.getElementById("tarefas");
     console.log(Tarefa[i])
     divTarefas.appendChild(paragrafo)
+    paragrafo.appendChild(fechar)
+
+    
+
+    function fecharBotao(){
+        
+        paragrafo.style.display = ("none")
+    }
+    
 }
+
+
+
+
 
 
